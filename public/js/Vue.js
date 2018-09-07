c346109cf081d8ea7698c7ab292cbb15a66e2551
Glitch (web-project-4a) <none>
2018-09-07 17:33:10 +0000

    🤒🏔 Updated with Glitch

---
+++ public/js/Vue.js
@@ -0,0 +1,13 @@
+var app21 = new Vue({
+	el: '#app-21',
+	data: {
+   	message: 'Welcome on our wepage we are the ' + new Date().toLocaleString()
+ 		}
+})
+
+var app22 = new Vue({
+	el: '#app-22',
+	data: {
+   	message: 'If you have any enquiries just ask us !'
+ 		}
+})
